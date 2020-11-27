a841dc191464a173612f8942cb11558ba233f02c
Samuel Stenberg <samuel.stenberg@teokem.lu.se>
2020-11-27 17:42:24 +0100

    - Code working - Optimizing

---
+++ DFT.jl
@@ -0,0 +1,325 @@
+include("Constants.jl")
+
+module DFT
+import ..D
+import ..Constants
+
+#Simulation Constants
+const T = 298.0
+const kT = T * Constants.k_B
+const beta = 1.0 / (kT)
+const ε_r = 78.3
+const l_B = Constants.e^2 / (4.0 * pi * Constants.ε_0 * ε_r) * beta * 10.0^(10.0)
+h = 20.0
+const sigma = -1.0 / 200.0
+const mix = 0.004
+const dz = 0.05
+const lambda = (8.0*pi*0.01)^(1.0/3.0)
+
+println("l_B is: $l_B")
+println("surfdens is: $sigma")
+
+function set_h(hz)
+    global h = hz
+end
+
+struct Energy{T<:Function}
+    phi::T
+    phiV::Vector{Float64}
+    ext::T
+    extV::Vector{Float64}
+    hole::T
+    holeVP::Vector{Float64}
+    holeVN::Vector{Float64}
+end
+
+
+function find_root3p(a, b, c, guess)
+    x0 = guess
+    x1 = 0.0
+    for i in 1:1000000
+        x1 = x0 - (b*x0^(4.0) + a*x0 + c) / (4.0*b*x0^(3.0) + a)
+
+        if(abs(x0 - x1) < 1e-10)
+            break
+        end
+        x0 = x1
+    end
+    return x1
+end
+
+function find_root1p(a, b, c, guess)
+    x0 = guess
+    x1 = 0.0
+    for i in 1:100
+        x1 = x0 - (b*x0^(2.0) + a*x0 + c) / (2.0*b*x0 + a)
+        if(abs(x0 - x1) < 1e-10)
+            break
+        end
+        x0 = x1
+    end
+    return x1
+end
+
+function phi(r, q1 = 1.0, q2 = 1.0)
+    #smphiz = 2.d0*pi*diffz*rrT*dz
+    return -2.0 * pi * l_B * q1 * q2 * r
+end
+
+
+function phi_coulomb(r, q1 = 1.0, q2 = 1.0)
+    return l_B * q1 * q2 / r
+end
+
+function external_diff(z, q = 1.0)
+    return -2.0 * pi * l_B * q * (sigma*z + sigma*(h - z))
+end
+
+
+function external(z, q = 1.0)
+    return -2.0 * pi * l_B * q * sigma * h
+    #return 0.0
+end
+
+
+
+function get_charge(p::Vector{Float64}, n::Vector{Float64}, pq, nq)
+    charge = (pq * sum(p) + nq * sum(n)) * dz
+    return charge + 2.0 * sigma
+end
+
+function hole(r::Float64)
+    return (1.0 - exp(-lambda * r))
+end
+
+function hole(r::Float64, lambda::Float64)
+    return (1.0 - exp(-lambda * r))
+end
+
+
+function hole_coloumb(diffz, lambda, q1 = 1.0)
+    return (-2.0*pi*l_B*q1^2.0 * (diffz +  exp(-lambda*diffz) / lambda))
+    #return energy
+end
+
+function hole_coloumb2(z, lambda, q1 = 1.0)
+    energy = -2.0 * pi * l_B * q1^2.0 * z * (1.0 - exp(-lambda * z))
+    return energy
+end
+
+function pre_cal(f, v::Vector{Float64})
+    for i in 0:dz:h
+        v[round(Int, i / dz) + 1] = f(i)
+    end
+end
+
+function pre_cal_hole(f, v::Vector{Float64}, lambda)
+    for i in 0:dz:h
+        v[round(Int, i / dz) + 1] = f(i, lambda)
+    end
+end
+
+function energy(n::Vector{Float64}, q1, q2, z, en::Energy)
+    energy = 0.0
+    
+    #Integrate
+    @simd for i in eachindex(n)
+        diffz = abs(z - (i*dz - dz))
+        @inbounds energy = energy + n[i] * phi(diffz, q1, q2) * dz
+        #energy = energy + n[i] * q1 * q2 * en.phiV[round(Int, abs(z - i*dz) / dz) + 1] * dz
+    end
+
+    return energy
+end
+
+function energy_wh(s::D, z, en::Energy)
+    energy = 0.0
+    
+    #lambda = sqrt(2.0*pi*abs(sigma) / abs(q1))
+    #Integrate
+    @simd for i in eachindex(s.n)
+        diffz = abs(z - (i*dz - dz))
+        #@inbounds energy = energy + n[i] * phi(diffz, q1, q2) * (1.0 - exp(-lambda * diffz)) * dz 
+        @inbounds energy = energy + s.n[i] * hole_coloumb(diffz, s.lambda, s.q) * dz 
+        #@inbounds energy = energy + n[i] * phi(abs(z - i*dz), q1, q2) * hole(diffz, lambda) * dz 
+        #-2.d0*pi*rrT*(diffz+dexp(-tlambda*diffz)/tlambda)*dz
+        #energy = energy - n[i]*2.0*pi*l_B*q1*q2 * (diffz -  exp(-lambda*diffz) / lambda) * dz 
+        #en.holeV[round(Int, abs(z - i*dz) / dz) + 1]
+        #energy = energy + n[i] * q1 * q2 * en.phiV[round(Int, abs(z - i*dz) / dz) + 1] * dz
+    end
+    
+    return energy
+end
+
+function energy_whp(s::D, z, en::Energy)
+    energy = 0.0
+    @simd for i in eachindex(s.n)
+        diffz = abs(z - (i*dz - dz))
+        @inbounds energy = energy + s.n[i] * s.q^2.0* en.holeVP[round(Int, diffz / dz) + 1] * dz
+    end
+    return energy
+end
+
+function energy_whn(s::D, z, en::Energy)
+    energy = 0.0
+    @simd for i in eachindex(s.n)
+        diffz = abs(z - (i*dz - dz))
+        @inbounds energy = energy + s.n[i] * s.q^2.0 * en.holeVN[round(Int, diffz / dz) + 1] * dz
+    end 
+    return energy
+end
+
+function update_densities!(p::D, n::D, donn::Float64, en::Energy)
+    global mix
+    pn_temp = fill(0.0, length(p.n))
+    nn_temp = fill(0.0, length(n.n))
+
+    Threads.@threads for i in eachindex(p.n)
+        z = i*dz - dz
+        pe = energy(n.n, p.q, n.q, z, en) + energy_whp(p, z, en)
+        ne = energy(p.n, n.q, p.q, z, en) + energy_whn(n, z, en)
+        @inbounds pn_temp[i] = exp(p.cp + p.q * donn - external(z, p.q) - pe)
+        @inbounds nn_temp[i] = exp(n.cp + n.q * donn - external(z, n.q) - ne)
+    end
+
+    charge = get_charge(pn_temp, nn_temp, p.q, n.q)
+
+    #Neutralize
+    if(abs(charge) > 1e-10)
+        dDonn = log(find_root3p(2.0*sigma, p.q*sum(pn_temp)*dz, n.q*sum(nn_temp)*dz, exp(donn)))
+        pn_temp = pn_temp .* exp(p.q * dDonn)
+        nn_temp = nn_temp .* exp(n.q * dDonn)
+        donn = donn + dDonn
+    end
+    p.n = (mix .* pn_temp .+ (1.0 - mix) .* p.n::Vector{Float64})
+    n.n = (mix .* nn_temp .+ (1.0 - mix) .* n.n::Vector{Float64})  
+    return donn
+end
+
+function get_f(p::D, n::D, en::Energy)
+    f = 0.0
+    
+    #for i in eachindex(p.n)
+    for i in 1:1:round(Int, length(p.n) / 2.0)
+        z  = i*dz - dz
+
+        f_temp = energy(n.n, p.q, n.q, z, en)          #+-
+        f_temp = f_temp + 0.5 * energy_wh(p, z, en) #++
+
+
+        f = f + p.n[i]*(f_temp + external(z, p.q))
+        f = f + p.n[i]*(log(p.n[i]) - 1.0 - p.cp)  #Ideal + legendre GC
+    end
+
+    #for i in eachindex(n.n)
+    for i in 1:1:round(Int, length(n.n) / 2.0)
+        z  = i*dz - dz
+
+        f_temp = 0.5 * energy_wh(n, z, en) #--
+
+
+        f = f + n.n[i]*(f_temp + external(z, n.q))
+        f = f + n.n[i]*(log(n.n[i]) - 1.0 - n.cp)  #Ideal + legendre GC
+
+        #fel = fds*(srnsval*(VL*z+VR*hz)+0.5d0*uss+Vexs(iz))
+        #felns = fds*(0.5d0*uss+Vexs(iz))
+        #aeta = pis*cdmonm(iz)
+        #rxsib = 1.d0/(1.d0-aeta)
+        #ccc = -fds+ccc
+        #sumFsns = fds*(dlog(fds)-1.d0-chemps)+felns+sumFsns
+        #sumFs = fds*(dlog(fds)-1.d0-chemps)+fel+sumFs
+    end
+
+    f = 2.0 * f * dz -2.0*pi*h * l_B * sigma^2.0
+    return f
+end
+
+function get_pressure(p::D, n::D, p_B)
+
+    fp1S = p.n[1]
+    fn1S = p.n[1+2]
+    c0Skv = p.n[1+1]
+    c1Skv = (fp1S-fn1S)*0.5
+    c2Skv = (fp1S+fn1S-2.0*c0Skv)*0.5
+#      ** EXTRAPOLATION BY USE OF QUADRATIC EXPRESSION **
+    fwcmq = (c0Skv+c1Skv*1.5+c2Skv*2.25)
+#      ** EXTRAPOLATION BY USE OF LINEAR EXPRESSION **
+    fwcml = fp1S+0.5*(fp1S-c0Skv)
+    if (fwcml < 1e-11) fwcmq = 0.0 end
+    #println("monomer contact density - quad. extr.: ", fwcmq)
+    
+    fp1S = n.n[1]
+    fn1S = n.n[1 + 2]
+    c0Skv = n.n[1 + 1]
+    c1Skv = (fp1S - fn1S)*0.5
+    c2Skv = (fp1S + fn1S - 2.0*c0Skv)*0.5
+#      ** EXTRAPOLATION BY USE OF QUADRATIC EXPRESSION **
+    fwcsq = (c0Skv + c1Skv*1.5 + c2Skv*2.25)
+    fwcsl = fp1S+0.5*(fp1S-c0Skv)
+    if (fwcsl < 1e-11) fwcsq = 0.0 end
+    #println("solvent contact density - quad. extr.: ", fwcsq)
+    VL = -2.0*pi*l_B*sigma
+
+    Psubnet = fwcmq + fwcsq - p_B
+    println("net solvation force (excl. csurf and surf.-surf.): ", Psubnet)
+
+    Pnet = Psubnet - (p.q*0.5*sum(p.n) + n.q*0.5*sum(n.n))*dz * VL
+
+    return Pnet
+end
+
+function run!(p::D, n::D, donn, en::Energy, iters)
+    es22mm = -4.0*pi*p.q*p.q*l_B/(p.lambda^(2.0))
+    es22ss = -4.0*pi*n.q*n.q*l_B/(n.lambda^(2.0))
+    p_B = p.dens+n.dens+0.5*es22mm*p.dens^(2.0)+0.5*es22ss*n.dens^(2.0)
+
+    println("+lambda: ", p.lambda)
+    println("-lambda: ", n.lambda)
+    println("+cp: ", p.cp)
+    println("-cp", n.cp)
+    println("Bulk pressure: ", p_B)
+
+    for i in 0:iters
+        charge = get_charge(p.n, n.n, p.q, n.q)
+        
+        if(i % 100 == 0)
+            println()
+            println("Iteration: $i")
+            println("Charge: $charge")
+            println("Donnan: $donn")
+            println("sum pn: ", sum(p.n))
+            println("sum nn: ", sum(n.n))
+            if(isnan(charge))
+                println("Charge is NaN exiting...")
+                break
+            end
+            #pressure = p.n[round(Int, 2.5 / dz)] + n.n[round(Int, 2.5 / dz)]
+            #println("Pressure: ", pressure)
+
+        end
+
+        if(abs(charge) > 1e-10)
+            dDonn = log(find_root3p(2.0*sigma, p.q * sum(p.n)*dz, n.q * sum(n.n)*dz, exp(donn)))
+            donn = donn + dDonn
+        end
+
+        donn = update_densities!(p, n, donn, en)
+
+        #Symmetrize
+        for i in 1:1:floor(Int, length(p.n) / 2.0)
+            p.n[i] = (p.n[i] + p.n[end-i+1]) / 2.0
+            p.n[end-i+1] = p.n[i]
+            n.n[i] = (n.n[i] + n.n[end-i+1]) / 2.0
+            n.n[end-i+1] = n.n[i]
+        end
+
+    end
+    dp = get_pressure(p, n, p_B)
+    gp = get_f(p, n, en)
+    println()
+    println("Grand Potential: ", gp)
+    println("Disjoining pressure: ", dp)
+    println("Final charge sum: ", p.q*sum(p.n)*DFT.dz + n.q*sum(n.n)*DFT.dz )
+    return donn, dp, gp
+end
+end
--- run.jl
+++ run.jl
@@ -1,257 +1,198 @@
 using PyPlot
 using DelimitedFiles
-
-module DFT
-import ..Constants
-#Simulation Constants
-const T = 298.0
-const kT = T * Constants.k_B
-const beta = 1.0 / (kT)
-const ε_r = 78.3
-const l_B = Constants.e^2 / (4.0 * pi * Constants.ε_0 * ε_r) * beta * 10^(10.0)
-const h = 20.0
-const sigma = -1.0 / 200.0
-const mix = 0.01
-const dz = 0.05
-const lambda = (8.0*pi*0.01)^(1.0/3.0)
-
-println("l_B is: $l_B")
-println("surfdens is: $sigma")
+include("Constants.jl")
+using .Constants
 
 mutable struct D
     n::Vector{Float64}
     q::Float64
     cp::Float64
-    l::Int32
+    dens::Float64
+    lambda::Float64
 end
 
+include("DFT.jl")
 
-struct Energy{T<:Function}
-    phi::T
-    phiV::Vector{Float64}
-    ext::T
-    extV::Vector{Float64}
-    hole::T
-    holeV::Vector{Float64}
+
+
+function main(sv)
+    ps = zeros(0)
+    gps = zeros(0)
+    for i in 1:length(sim_vars["conc"])
+        DFT.set_h(sv["h"][i])
+        println("h: ", sv["h"][i])
+        pq = 3.0
+        conc = sv["conc"][i] #Molars
+        donnan = sv["donnan"][i];
+        dens = conc * Constants.N_A * 10^(-27.0)
+        pcp = log(dens)
+        ncp = log(pq * dens)
+        p_lambda = (2.0*Constants.pi*abs(DFT.sigma)/pq)^0.5
+        n_lambda = (2.0*Constants.pi*abs(DFT.sigma))^0.5 
+        pcp = pcp - 4.0*DFT.pi*pq*pq*DFT.l_B/p_lambda^2.0 * dens
+        ncp = ncp - 4.0*DFT.pi    *  DFT.l_B/n_lambda^2.0 * pq * dens
+
+                                        #Density                  q    cp        l         
+        p = D(fill(dens *DFT.h, round(Int, DFT.h / DFT.dz) + 1), 
+            pq,   pcp, dens, p_lambda)
+        n = D(fill(pq * dens * DFT.h, round(Int, DFT.h / DFT.dz) + 1), -1.0, 
+            ncp, pq * dens, n_lambda)
+
+        en = DFT.Energy{Function}(DFT.phi, fill(0.0, length(p.n)), DFT.external, fill(0.0, length(p.n)),
+            DFT.hole_coloumb, fill(0.0, length(p.n)), fill(0.0, length(n.n)))
+        DFT.pre_cal_hole(en.hole, en.holeVP, p.lambda)
+        DFT.pre_cal_hole(en.hole, en.holeVN, n.lambda)
+
+        DFT.pre_cal(en.phi, en.phiV)
+        DFT.pre_cal(en.ext, en.extV)
+        
+        #println(en.phiV)
+        #println(isabstracttype(en))
+        @time donnan, dp, gp = DFT.run!(p, n, donnan, en, 3000)
+        append!(ps, dp)
+        append!(gps, gp)
+        #PLOTTING
+        zs = [0.0:DFT.dz:DFT.h;];
+
+        data = reshape(readdlm("../../Downloads/jan_DFT/fcdfil"),:,3)
+        
+        PyPlot.clf()
+        
+        plot(data[:,1], data[:,2], color="green", linewidth=2.0, linestyle="-")
+        #plot(data[:,1], data[:,3], color="blue", linewidth=2.0, linestyle="-")
+        
+        plot(zs, p.n, color="red", linewidth=1.0, linestyle="-")
+        xlim(0, 20.0)
+        #plot(zs, n.n, color="black", linewidth=1.0, linestyle="-")
+        plot(size=(200,200))
+        display(gcf())
+    end 
+    return ps, gps
 end
 
+#mutable struct Result
+#    h::Float64
+#    conc::Float64
+#    donnan::Float64
+#    dp::Float64
+#    gp::Float64
+#end
+#
+#function main2(result::Result)
+#    DFT.set_h(result.h)
+#    println("h: ", result.h)
+#    pq = 3.0
+#    conc = result.conc #Molars
+#    donnan = result.donnan;
+#    dens = conc * Constants.N_A * 10^(-27.0)
+#    pcp = log(dens)
+#    ncp = log(pq * dens)
+#    p_lambda = (2.0*Constants.pi*abs(DFT.sigma)/pq)^0.5
+#    n_lambda = (2.0*Constants.pi*abs(DFT.sigma))^0.5 
+#    pcp = pcp - 4.0*DFT.pi*pq*pq*DFT.l_B/p_lambda^2.0 * dens
+#    ncp = ncp - 4.0*DFT.pi    *  DFT.l_B/n_lambda^2.0 * pq * dens
+#
+#                                    #Density                  q    cp        l         
+#    p = D(fill(conc * dens *DFT.h, round(Int, DFT.h / DFT.dz) + 1), 
+#        pq,   pcp, dens, p_lambda)
+#    n = D(fill(pq * dens * DFT.h, round(Int, DFT.h / DFT.dz) + 1), -1.0, 
+#        ncp, pq * dens, n_lambda)
+#
+#    en = DFT.Energy{Function}(DFT.phi, fill(0.0, length(p.n)), DFT.external, fill(0.0, length(p.n)),
+#        DFT.hole, fill(0.0, length(p.n)))
+#    DFT.pre_cal(en.hole, en.holeV)
+#    DFT.pre_cal(en.phi, en.phiV)
+#    DFT.pre_cal(en.ext, en.extV)
+#
+#    #println(en.phiV)
+#    #println(isabstracttype(en))
+#    @time donnan, dp, gp = DFT.run!(p, n, donnan, en, 10000)
+#    result.dp = dp
+#    result.gp = gp
+#end
 
-function find_root3p(a, b, c, guess)
-    x0 = guess
-    x1 = 0.0
-    for i in 1:1000000
-        x1 = x0 - (b*x0^(4.0) + a*x0 + c) / (4.0*b*x0^(3.0) + a)
 
-        if(abs(x0 - x1) < 1e-10)
-            break
-        end
-        x0 = x1
+concs = fill(0.001, 11)
+hs = 10.0:2.0:30.0
+donnans = 5.0:1.0:15.0
+println(length(concs))
+println(length(hs))
+println(length(donnans))
+sim_vars = Dict("conc"=>concs, 
+                "h"=>hs,
+                "donnan" => donnans)
+
+#resVec = Vector{Result}(undef, length(sim_vars["conc"]))
+#
+#for i in 1:length(sim_vars["conc"])
+#    #res = Dict("conc"=>sim_vars["conc"][i], "h"=>sim_vars["h"][i], "donnan"=>sim_vars["donnan"][i])
+#    resVec[i] = Result(sim_vars["h"][i], sim_vars["conc"][i], sim_vars["donnan"][i], 0.0, 0.0)
+#    t = @task begin
+#        main2(resVec[i])
+#    end
+#
+#    schedule(t)
+#
+#end
+#@sync println("Finished")
+#for t in tasks
+#    wait(t)
+#end
+
+ps, gps = main(sim_vars)
+
+using DelimitedFiles
+using Interpolations
+
+function pl(ps, gps)
+    der = zeros(0)
+    xs = zeros(0)
+    for i in 1:1:length(hs) - 1
+        dyf = (gps[i + 1] + gps[i]) / 2.0 - gps[i + 1]
+        dyb = (gps[i + 1] + gps[i]) / 2.0 - gps[i]
+        #x = (hs[i + 1] + hs[i]) / 2.0
+        x = (hs[i + 1] + hs[i]) / 2.0
+        dxf = x - hs[i + 1]
+        dxb = x - hs[i]
+        dxyf = -dyf / dxf
+        dxyb = -dyb / dxb
+        append!(der, (dxyf + dxyb) / 2.0)
+        append!(xs, x)
     end
-    return x1
+
+    A = gps
+    A_x = 10.0:2.0:30.0
+    nodes = (A_x,)
+    itp = interpolate(nodes, A, Gridded(Linear()))
+
+    dgps = -diff(gps) ./ diff(hs)
+
+    PyPlot.clf()
+    plot(hs, ps, linewidth=2.0, color="red")
+
+    #plot(hs[1:end-1], dgps)
+    plot(xs, der, linewidth=2.0)
+    #plot(xs[2:end], dgps2, color="green")
+    #plot(hs[1:end], gps, color="blue")
+    #plot(xs, itp(xs), color="red", linewidth=2.0, linestyle="--")
+    xlim(10.0, 30.0)
+    display(gcf())
+
+    savefig("../../Documents/dft_prog/dis_press.png")
 end
 
-function find_root1p(a, b, c, guess)
-    x0 = guess
-    x1 = 0.0
-    for i in 1:100
-        x1 = x0 - (b*x0^(2.0) + a*x0 + c) / (2.0*b*x0 + a)
-        if(abs(x0 - x1) < 1e-10)
-            break
-        end
-        x0 = x1
-    end
-    return x1
+open("../../Documents/dft_prog/grand_potential.txt", "w") do io
+    writedlm(io, [hs gps])
 end
-
-function phi(r, q1 = 1.0, q2 = 1.0)
-    #smphiz = 2.d0*pi*diffz*rrT*dz
-    return -2.0 * pi * l_B * q1 * q2 * r
-end
-
-
-function phi_coulomb(r, q1 = 1.0, q2 = 1.0)
-    return l_B * q1 * q2 / r
-end
-
-function external_diff(z, q = 1.0)
-    return -2.0 * pi * l_B * q * (sigma*z + sigma*(h - z))
-end
-
-
-function external(z, q = 1.0)
-    return -2.0 * pi * l_B * q * sigma * h
-    #return 0.0
+open("../../Documents/dft_prog/Pnet.txt", "w") do io
+    writedlm(io, [hs ps])
 end
 
 
 
-function get_charge(p::Vector{Float64}, n::Vector{Float64}, pq, nq)
-    charge = (pq * sum(p) + nq * sum(n)) * dz
-    return charge + 2.0 * sigma
-end
-
-function hole(r::Float64)
-    return (1.0 - exp(-lambda * r))
-end
-
-function hole(r::Float64, lambda::Float64)
-    return (1.0 - exp(-lambda * r))
-end
 
 
-function hole_coloumb(diffz, lambda, q1 = 1.0, q2 = 1.0)
-    energy = -2.0*pi*l_B*q1*q2 * (diffz +  exp(-lambda*diffz) / lambda)
-    return energy
-end
 
-function hole_coloumb2(z, lambda, q1 = 1.0, q2 = 1.0)
-    energy = -2.0 * pi * l_B * q1 * q2 * z * (1.0 - exp(-lambda * z))
-    return energy
-end
-
-function pre_cal(f, v::Vector{Float64})
-    for i in 0:dz:h
-        v[round(Int, i / dz) + 1] = f(i)
-    end
-end
-
-function energy(n::Vector{Float64}, q1, q2, z, en::Energy)
-    energy = 0.0
-    
-    #Integrate
-    @simd for i in eachindex(n)
-        diffz = abs(z - (i*dz - dz))
-        @inbounds energy = energy + n[i] * phi(diffz, q1, q2) * dz
-        #energy = energy + n[i] * q1 * q2 * en.phiV[round(Int, abs(z - i*dz) / dz) + 1] * dz
-    end
-
-    return energy
-end
-
-function energy_wh(n::Vector{Float64}, q1, q2, z, en::Energy)
-    energy = 0.0
-    
-    lambda = sqrt(2.0*pi*abs(sigma) / abs(q1))
-    #Integrate
-    @simd for i in eachindex(n)
-        diffz = abs(z - (i*dz - dz))
-        #@inbounds energy = energy + n[i] * phi(diffz, q1, q2) * (1.0 - exp(-lambda * diffz)) * dz 
-        @inbounds energy = energy + n[i] * hole_coloumb(diffz, lambda, q1, q2) * dz 
-        #@inbounds energy = energy + n[i] * phi(abs(z - i*dz), q1, q2) * hole(diffz, lambda) * dz 
-        #-2.d0*pi*rrT*(diffz+dexp(-tlambda*diffz)/tlambda)*dz
-        #energy = energy - n[i]*2.0*pi*l_B*q1*q2 * (diffz -  exp(-lambda*diffz) / lambda) * dz 
-        #en.holeV[round(Int, abs(z - i*dz) / dz) + 1]
-        #energy = energy + n[i] * q1 * q2 * en.phiV[round(Int, abs(z - i*dz) / dz) + 1] * dz
-    end
-    
-    return energy
-end
-
-
-function update_densities!(p::D, n::D, donn::Float64, en::Energy)
-    global mix
-    pn_temp = fill(0.0, p.l)
-    nn_temp = fill(0.0, n.l)
-
-    @simd for i in eachindex(p.n)
-        z = i*dz - dz
-        pe = energy(n.n, p.q, n.q, z, en) + energy_wh(p.n, p.q, p.q, z, en)
-        ne = energy(p.n, n.q, p.q, z, en) + energy_wh(n.n, n.q, n.q, z, en)
-        @inbounds pn_temp[i] = exp(p.cp + p.q * donn - external(z, p.q) - pe)
-        @inbounds nn_temp[i] = exp(n.cp + n.q * donn - external(z, n.q) - ne)
-    end
-
-    charge = get_charge(pn_temp, nn_temp, p.q, n.q)
-
-    #Neutralize
-    if(abs(charge) > 1e-10)
-        dDonn = log(find_root3p(2.0*sigma, p.q*sum(pn_temp)*dz, n.q*sum(nn_temp)*dz, exp(donn)))
-        pn_temp = pn_temp .* exp(p.q * dDonn)
-        nn_temp = nn_temp .* exp(n.q * dDonn)
-        donn = donn + dDonn
-    end
-    p.n = (mix .* pn_temp .+ (1.0 - mix) .* p.n::Vector{Float64})
-    n.n = (mix .* nn_temp .+ (1.0 - mix) .* n.n::Vector{Float64})  
-    return donn
-end
-
-
-function run!(p::D, n::D, donn, en::Energy)
-    for i in 0:2000
-        charge = get_charge(p.n, n.n, p.q, n.q)
-        if(i % 100 == 0)
-            println()
-            println("Iteration: $i")
-            println("Charge: $charge")
-            println("Donnan: $donn")
-            println("sum pn: ", sum(p.n))
-            println("sum nn: ", sum(n.n))
-            if(isnan(charge))
-                println("Charge is NaN exiting...")
-                break
-            end
-        end
-
-        if(abs(charge) > 1e-10)
-            dDonn = log(find_root3p(2.0*sigma, p.q * sum(p.n)*dz, n.q * sum(n.n)*dz, exp(donn)))
-            donn = donn + dDonn
-
-            #p.n = p.n .* exp(p.q * dDonn)
-            #n.n = n.n .* exp(n.q * dDonn)
-        end
-
-        donn = update_densities!(p, n, donn, en)
-
-    end
-    return donn
-end
-end
-
-pq = 3.0
-conc = 0.001 #Molars
-donnan = 5.0;
-
-dens = conc * Constants.N_A * 10^(-27.0)
-pcp = log(dens)
-ncp = log(pq * dens)
-
-p_lambda = (2.0*Constants.pi*abs(DFT.sigma)/pq)^0.5
-n_lambda = (2.0*Constants.pi*abs(DFT.sigma))^0.5 
-println("p_lambda: $p_lambda")
-println("n_lambda: $n_lambda")
-pcp = pcp - 4.0*DFT.pi*pq*pq*DFT.l_B/p_lambda^2.0 * dens
-ncp = ncp - 4.0*DFT.pi    *  DFT.l_B/n_lambda^2.0 * pq * dens
-println("Cation chemical potential is: ", pcp)
-println("Anion chemical potential is: ", ncp)
-                                  #Density                  q    cp        l         
-p = DFT.D(fill(conc * Constants.N_A * 10^(-27.0)*DFT.h, round(Int, DFT.h / DFT.dz) + 1), pq,   pcp, round(Int,DFT. h / DFT.dz) + 1)
-n = DFT.D(fill(pq * conc * Constants.N_A * 10^(-27.0)*DFT.h, round(Int, DFT.h / DFT.dz) + 1), -1.0, ncp, round(Int, DFT.h / DFT.dz) + 1)
-en = DFT.Energy{Function}(DFT.phi, fill(0.0, p.l), DFT.external, fill(0.0, p.l), DFT.hole, fill(0.0, p.l))
-DFT.pre_cal(en.hole, en.holeV)
-DFT.pre_cal(en.phi, en.phiV)
-DFT.pre_cal(en.ext, en.extV)
-
-#println(en.phiV)
-#println(isabstracttype(en))
-@time donnan = DFT.run!(p, n, donnan, en)
-println("Final charge sum: ", p.q*sum(p.n)*DFT.dz + n.q*sum(n.n)*DFT.dz )
-
-
-#PLOTTING
-zs = [0.0:DFT.dz:DFT.h;];
-
-data = reshape(readdlm("../../Downloads/jan_DFT/fcdfil"),:,3)
-
-PyPlot.clf()
-
-plot(data[:,1], data2[:,2], color="green", linewidth=2.0, linestyle="-")
-plot(data[:,1], data2[:,3], color="blue", linewidth=2.0, linestyle="-")
-
-plot(zs, p.n, color="red", linewidth=1.0, linestyle="-")
-plot(zs, n.n, color="black", linewidth=1.0, linestyle="-")
-xlim(0, 20)
-plot(size=(200,200))
-display(gcf())
 
 
 #plot(zs, nn, color="blue", linewidth=1.0, linestyle="-")
@@ -264,7 +205,19 @@ display(gcf())
 #
 #DFT.phi(DFT.dz) * DFT.dz + DFT.hole(DFT.dz, lambda) * DFT.dz
 #println("+-: ",-DFT.phi(DFT.dz * DFT.dz, 3.0, -1.0))
-#
+##
 #println("--: ", DFT.hole_coloumb(DFT.dz, lambda, -1.0, -1.0) * DFT.dz )
 #lambda = sqrt(2.0*pi*abs(sigma) / 3.0)
 #println("++: ", DFT.hole_coloumb(DFT.dz, lambda, 3.0, 3.0) * DFT.dz)
+#
+#data = reshape(readdlm("../../Downloads/jan_DFT/fcdfil"),:,3)
+#data2 = reshape(readdlm("../../Downloads/jan_DFT/fcdfil_1"),:,3)
+#    
+#PyPlot.clf()
+#
+#plot(data[:,1], data2[:,2], color="green", linewidth=2.0, linestyle="-")
+#plot(data[:,1], data2[:,3], color="blue", linewidth=2.0, linestyle="-")
+#
+#plot(data2[:,1], data2[:,2], linewidth=1.0, linestyle="-")
+#plot(data2[:,1], data2[:,3], linewidth=1.0, linestyle="-")
+#display(gcf())
